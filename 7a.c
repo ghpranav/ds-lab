892beeccc812051508b123a4d639168d79fa875e
Pranav Bedre <bedrepranav@gmail.com>
2019-09-04 08:34:26 +0530

    Create 7a.c

---
+++ 7a.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int fact(int n) {
+	if(n == 1)
+		return 1;
+	else 
+		return n*fact(n-1);
+}
+
+int main() {
+	int n, fact(int);
+	printf("Enter the number:\n");
+	scanf("%d",&n);
+	printf("%d! = %d\n", n, fact(n));
+	return 0;
+}
